444831a863121afa5ae86009819596268fe397bf
ajlopez <webmaster@ajlopez.com>
2017-07-29 13:38:22 -0300

    Decompile mstore

--- test/bytecodes.js
+++ test/bytecodes.js
@@ -26,3 +26,20 @@ exports['decompile two push1'] = function (test) {
 	test.equal(result[1].value, '0x40');
 };
 
+exports['decompile two push1 and mstore'] = function (test) {
+	var result = bc.decompile('6060604052');
+	
+	test.ok(result);
+	test.ok(Array.isArray(result));
+	test.equal(result.length, 3);
+	
+	test.equal(result[0].opcode, 'push1');
+	test.equal(result[0].value, '0x60');
+	
+	test.equal(result[1].opcode, 'push1');
+	test.equal(result[1].value, '0x40');
+	
+	test.equal(result[2].opcode, 'mstore');
+	test.equal(result[2].value, null);
+};
+
