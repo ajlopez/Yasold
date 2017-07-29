62e990034b70a16ab70fa0c276bdc833d8417951
ajlopez <webmaster@ajlopez.com>
2017-07-29 13:42:03 -0300

    Decompile callvalue

--- test/bytecodes.js
+++ test/bytecodes.js
@@ -43,3 +43,23 @@ exports['decompile two push1 and mstore'] = function (test) {
 	test.equal(result[2].value, null);
 };
 
+exports['decompile two push1, mstore and callvalue'] = function (test) {
+	var result = bc.decompile('606060405234');
+	
+	test.ok(result);
+	test.ok(Array.isArray(result));
+	test.equal(result.length, 4);
+	
+	test.equal(result[0].opcode, 'push1');
+	test.equal(result[0].value, '0x60');
+	
+	test.equal(result[1].opcode, 'push1');
+	test.equal(result[1].value, '0x40');
+	
+	test.equal(result[2].opcode, 'mstore');
+	test.equal(result[2].value, null);
+	
+	test.equal(result[3].opcode, 'callvalue');
+	test.equal(result[3].value, null);
+};
+
