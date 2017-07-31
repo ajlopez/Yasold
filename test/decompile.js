de1cab1f2c01ec4d82e5e519997a06ebf3b2f9b5
ajlopez <webmaster@ajlopez.com>
2017-07-31 17:52:05 -0300

    Decompile with offset

--- test/decompile.js
+++ test/decompile.js
@@ -12,4 +12,24 @@ exports['decompile push1'] = function (test) {
 	test.equal(result[0].value, '0x60');
 };
 
+exports['decompile two push1 and mstore and calculate offsets'] = function (test) {
+	var result = yasold.decompile('6060604052');
+	
+	test.ok(result);
+	test.ok(Array.isArray(result));
+	test.equal(result.length, 3);
+	
+	test.equal(result[0].opcode, 'push1');
+	test.equal(result[0].value, '0x60');
+	test.equal(result[0].offset, 0);
+	
+	test.equal(result[1].opcode, 'push1');
+	test.equal(result[1].value, '0x40');
+	test.equal(result[1].offset, 2);
+	
+	test.equal(result[2].opcode, 'mstore');
+	test.equal(result[2].value, null);
+	test.equal(result[2].offset, 4);
+};
+
 
