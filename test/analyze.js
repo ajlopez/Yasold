72e1be5a475d57366f5068f29900410d631df25e
ajlopez <webmaster@ajlopez.com>
2017-07-31 18:58:09 -0300

    Analyze first test

---
+++ test/analyze.js
@@ -0,0 +1,24 @@
+
+var yasold = require('..');
+
+exports['analyze with opcodes'] = function (test) {
+	var result = yasold.analyze('6060604052');
+	
+	test.ok(result);
+	test.ok(result.opcodes);
+	test.ok(Array.isArray(result.opcodes));
+	test.equal(result.opcodes.length, 3);
+	
+	test.equal(result.opcodes[0].opcode, 'push1');
+	test.equal(result.opcodes[0].value, '0x60');
+	test.equal(result.opcodes[0].offset, 0);
+	
+	test.equal(result.opcodes[1].opcode, 'push1');
+	test.equal(result.opcodes[1].value, '0x40');
+	test.equal(result.opcodes[1].offset, 2);
+	
+	test.equal(result.opcodes[2].opcode, 'mstore');
+	test.equal(result.opcodes[2].value, null);
+	test.equal(result.opcodes[2].offset, 4);
+};
+
