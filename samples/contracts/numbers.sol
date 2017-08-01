9bc6a639b05ed1154c2d7bdaac4f92b03a0fb227
ajlopez <webmaster@ajlopez.com>
2017-08-01 14:32:25 -0300

    Numbers contract

---
+++ samples/contracts/numbers.sol
@@ -0,0 +1,21 @@
+
+pragma solidity ^0.4.9;
+
+contract Number {
+	int private number;
+	
+	function Number() {
+		number = 42;
+	}
+	
+	function getNumber() returns (int) {
+		return number;
+	}
+	
+	function compare(int n) returns (bool) {
+		return n > number;
+	}
+}
+
+
+
