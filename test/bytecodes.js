6023f06fefd263c788ca8b107fdffa4e3b21eb73
ajlopez <webmaster@ajlopez.com>
2017-08-05 17:20:24 -0300

    Decompile sha3

--- test/bytecodes.js
+++ test/bytecodes.js
@@ -480,4 +480,15 @@ exports['decompile sstore'] = function (test) {
 	test.equal(result[0].value, null);
 };
 
+exports['decompile sha3'] = function (test) {
+	var result = bc.decompile('20');
+	
+	test.ok(result);
+	test.ok(Array.isArray(result));
+	test.equal(result.length, 1);
+	
+	test.equal(result[0].opcode, 'sha3');
+	test.equal(result[0].value, null);
+};
+
 
