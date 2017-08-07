b626f8672b8b0b5c73872cc4eeecd45269dd4f95
ajlopez <webmaster@ajlopez.com>
2017-08-07 16:13:46 -0300

    Analyze executing opcodes, populating stack, WIP

--- test/analyze.js
+++ test/analyze.js
@@ -22,6 +22,31 @@ exports['analyze with opcodes'] = function (test) {
 	test.equal(result.opcodes[2].offset, 4);
 };
 
+exports['analyze with stack'] = function (test) {
+	var result = yasold.analyze('6060604052');
+	
+	test.ok(result);
+	test.ok(result.opcodes);
+	test.ok(Array.isArray(result.opcodes));
+	test.equal(result.opcodes.length, 3);
+	
+	test.equal(result.opcodes[0].opcode, 'push1');
+	test.ok(result.opcodes[0].stack);
+	test.ok(result.opcodes[0].stack.length);
+	test.equal(result.opcodes[0].stack.length, 1);
+	test.equal(result.opcodes[0].stack[0], '0x60');
+	
+	test.equal(result.opcodes[1].opcode, 'push1');
+	test.ok(result.opcodes[1].stack);
+	test.ok(result.opcodes[1].stack.length);
+	test.equal(result.opcodes[1].stack.length, 1);
+	test.equal(result.opcodes[1].stack[0], '0x40');
+	
+	test.equal(result.opcodes[2].opcode, 'mstore');
+	test.ok(result.opcodes[1].stack);
+	test.equal(result.opcodes[1].stack.length, 0);
+};
+
 exports['analyze recognizes memory init'] = function (test) {
 	var result = yasold.analyze('6060604052');
 	
