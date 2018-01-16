cd1ba3803bcc2ba0e5daa5291a41ed988276921a
ajlopez <webmaster@ajlopez.com>
2018-01-16 10:32:26 -0300

    Decompile number opcode

--- test/bytecodes.js
+++ test/bytecodes.js
@@ -647,5 +647,16 @@ exports['decompile gaslimit'] = function (test) {
 	test.equal(result[0].value, null);
 };
 
+exports['decompile number'] = function (test) {
+	var result = bc.decompile('43');
+	
+	test.ok(result);
+	test.ok(Array.isArray(result));
+	test.equal(result.length, 1);
+	
+	test.equal(result[0].opcode, 'number');
+	test.equal(result[0].value, null);
+};
+
 
 
