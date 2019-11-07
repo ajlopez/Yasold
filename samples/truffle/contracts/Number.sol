f70fce5c381eee803a5dfdde721c58c2f7b0e91f
ajlopez <webmaster@ajlopez.com>
2019-11-07 14:13:41 -0300

    New contracts in truffle sample

---
+++ samples/truffle/contracts/Number.sol
@@ -0,0 +1,13 @@
+
+pragma solidity >=0.5.0 <0.6.0;
+
+contract Number {
+	int private number;
+	
+    constructor() public {
+		number = 42;
+	}
+}
+
+
+
---
+++ samples/truffle/contracts/Variable.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+contract Variable {
+	string private message;
+	
+	constructor() public {
+		message = "Hello, world";
+	}
+}
+
+
+
